92751bedad6034fee7e696aed7ed6b758e0108a5
Gab071 <kaminskigabriel07@gmail.com>
2026-09-07 14:19:26 +0300

    added dead zone

--- JoystickLedController/JoystickLedController.ino
+++ JoystickLedController/JoystickLedController.ino
@@ -6,23 +6,28 @@
 #define YPin A0
 #define SW 2
 
+// variable is preventing Led's from flickering
+// when joystick is in "resting" position (not moved)
+const int deadZone = 10; 
+
+// Variables that store LED's state 
 int ledR = 0;
 int ledB = 0;
 int ledY = 0;
 int ledG = 0;
-int neutralX = 0;
-int neutralY = 0;
+
+// Variables are initialized with joystick's "resting" position (not moved)
+int neutralX = analogRead(XPin);
+int neutralY = analogRead(YPin);
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(SW, INPUT);
+  Serial.begin(9600); // for debugging purposes
+  pinMode(SW, INPUT); // switch connected just in case
+
   pinMode(LedRed, OUTPUT);
   pinMode(LedBlue, OUTPUT);
   pinMode(LedYellow, OUTPUT);
   pinMode(LedGreen, OUTPUT);
-
-  neutralX = analogRead(XPin);
-  neutralY = analogRead(YPin);
 }
 
 void loop() {
@@ -38,16 +43,25 @@ void loop() {
   analogWrite(LedYellow, ledY);
 }
 
+
+// Function assigns for the LED's corresponding states depending on joystick position
+// led1 is the variable of the LED's state that gives bigger value when the joystick gives bigger value than neutral
+// led2 is the variable of the LED's state that gives bigger value when the joystick gives lesser value than neutral
 void SetLedState(int coordinate, int neutral, int &led1, int &led2)
 {
-  if(coordinate >= neutral)
+  if(coordinate >= neutral + deadZone)
   {
     led2 = 0;
     led1 = map(coordinate, neutral, 1023, 0, 255);
   }
-  else
+  else if(coordinate <= neutral - deadZone)
   {
     led1 = 0;
     led2 = map(coordinate, neutral, 0, 0, 255);
+  }
+  else
+  {
+    led1 = 0;
+    led2 = 0;
   }
 }
