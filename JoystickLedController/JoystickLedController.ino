022911198ff4fc1205e46d5cd9108bc850bf267f
Gab071 <kaminskigabriel07@gmail.com>
2026-09-08 14:51:19 +0300

    added joystick's switch functionality

--- JoystickLedController/JoystickLedController.ino
+++ JoystickLedController/JoystickLedController.ino
@@ -1,6 +1,7 @@
 #define LedRed 11
 #define LedBlue 10
 #define LedYellow 9
+#define LedWhite 7
 #define LedGreen 6
 #define XPin A1
 #define YPin A0
@@ -8,44 +9,48 @@
 
 void SetLedState(int coordinate, int neutral, int &led1, int &led2);
 
-
 // variable is preventing Led's from flickering
 // when joystick is in "resting" position (not moved)
-const int deadZone = 10; 
+const int deadZone = 20; 
 
-// Variables that store LED's state 
-int ledR = 0;
-int ledB = 0;
-int ledY = 0;
-int ledG = 0;
-
-// Variables are initialized with joystick's "resting" position (not moved)
-int neutralX = analogRead(XPin);
-int neutralY = analogRead(YPin);
-
-void setup() {
+void setup() 
+{
   Serial.begin(9600); // for debugging purposes
-  pinMode(SW, INPUT); // switch connected just in case
+  pinMode(SW, INPUT_PULLUP); // switch connected just in case
 
   pinMode(LedRed, OUTPUT);
   pinMode(LedBlue, OUTPUT);
   pinMode(LedYellow, OUTPUT);
   pinMode(LedGreen, OUTPUT);
+  pinMode(LedWhite, OUTPUT);
 }
 
-void loop() {
+void loop() 
+{
+  // Variables are initialized with joystick's "resting" position (not moved)
+  static const int neutralX = analogRead(XPin);
+  static const int neutralY = analogRead(YPin);
+
   int X = analogRead(XPin);
   int Y = analogRead(YPin);
+  int Switch = digitalRead(SW);
+
+  // Variables that store LED's state 
+  static int ledR = 0;
+  static int ledB = 0;
+  static int ledY = 0;
+  static int ledG = 0;
 
   SetLedState(X, neutralX, ledG, ledR);
   SetLedState(Y, neutralY, ledY, ledB);
+
+  (Switch == LOW) ? digitalWrite(LedWhite, HIGH) : digitalWrite(LedWhite, LOW);
 
   analogWrite(LedRed, ledR);
   analogWrite(LedBlue, ledB);
   analogWrite(LedGreen, ledG);
   analogWrite(LedYellow, ledY);
 }
-
 
 // Function assigns for the LED's corresponding states depending on joystick position
 // led1 is the variable of the LED's state that gives bigger value when the joystick gives bigger value than neutral
